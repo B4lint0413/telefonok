014d1be4971c593bef47b96d13b1b29041676c10
B4lint0413 <balu050413@gmail.com>
2024-10-07 12:00:50 +0000

    phone list created

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhoneController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PhoneController::class, "index"])->name("home");
+Route::get('/{id}', [PhoneController::class, "show"])->name("phones.show");
